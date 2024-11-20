4ad3a4d8ef19bae2c2974c8792d923a7d52fc0bd
mrazduev <mrazduev@gmail.com>
2024-11-20 10:05:57 +0300

    Module 4.1. Task 5. Switch

--- src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
@@ -1,6 +1,15 @@
 package school.mjc.stage0.conditions.task5;
 
 public class LeapYearPrinter {
-    public void isLeapYear(int year) {
+    public static void isLeapYear(int year) {
+        int leapStatus = (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0) ? 1 : 0;
+
+        switch (leapStatus){
+            case 1:
+                System.out.println("leap");
+                break;
+            case 0:
+                System.out.println("not leap");
+        }
     }
 }
